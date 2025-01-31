54be3e7ed5dd498bc7ae39f70466731854766fa3
Eduardo <eduardolagonunes@gmail.com>
2025-01-31 16:32:07 -0300

    REFACTOR: retirado modulo feed, sem uso

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,6 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { RegisterModule } from './register/register.module';
-import { FeedModule } from './feed/feed.module';
 import { PostsModule } from './post-with-image/post-with-image.module';
 import { ChatController } from './chat/chat.controller';
 import { ChatService } from './chat/chat.service';
@@ -11,7 +10,7 @@ import { ChatModule } from './chat/chat.module';
 import { ProfileModule } from './profile/profile.module';
 
 @Module({
-  imports: [AuthModule, RegisterModule, FeedModule, PostsModule, ChatModule, ProfileModule],
+  imports: [AuthModule, RegisterModule, PostsModule, ChatModule, ProfileModule],
   controllers: [AppController, ChatController],
   providers: [AppService, ChatService],
 })
