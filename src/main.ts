3062676e0899eac6fe9adca7c40b9949a0f46514
Eduardo <eduardolagonunes@gmail.com>
2025-02-10 14:01:35 -0300

    FEAT: habilitando netlify no CORS

--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['http://localhost:3000'],
-    methods: 'GET,POST,PUT,DELETE',
-    allowedHeaders: 'Content-Type, Authorization',
+    origin: ['http://localhost:3000', 'https://zanzar.netlify.app'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
   });
 
   await app.listen(3001);
