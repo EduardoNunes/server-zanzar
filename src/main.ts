a34d51ff152da4fbc724f978d86e7bb033cf56ee
Eduardo <eduardolagonunes@gmail.com>
2025-02-10 15:59:25 -0300

    FEAT: adicionando mais endereços

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,12 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['http://localhost:3000', 'https://zanzar.netlify.app'],
+    origin: [
+      'https://zanzar.netlify.app', // URL do frontend hospedado no Netlify
+      'capacitor://localhost', // Para o emulador do Capacitor
+      'http://localhost:3000', // Caso esteja rodando localmente o frontend
+      'https://server-zanzar.onrender.com', // Caso esteja rodando na produção, coloque o domínio aqui
+    ],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
