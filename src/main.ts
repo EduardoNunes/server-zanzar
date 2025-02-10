f83433ecc696d9548f6cc2eabf4a7bbd19ce14b0
Eduardo <eduardolagonunes@gmail.com>
2025-02-10 16:00:14 -0300

    ...

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,6 @@ async function bootstrap() {
       'https://zanzar.netlify.app', // URL do frontend hospedado no Netlify
       'capacitor://localhost', // Para o emulador do Capacitor
       'http://localhost:3000', // Caso esteja rodando localmente o frontend
-      'https://server-zanzar.onrender.com', // Caso esteja rodando na produção, coloque o domínio aqui
     ],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
