e21495549ae1d2b336654fa3b4ec63c99c5edf4a
Eduardo <eduardolagonunes@gmail.com>
2025-05-08 21:00:21 -0300

    FEAT: add column avaliableQuantity, priceAtPurchaseBase, totalFee

---
+++ prisma/migrations/20250507212049_add_column_total_fee/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "UserStore" ADD COLUMN     "totalFee" INTEGER NOT NULL DEFAULT 0;
---
+++ prisma/migrations/20250508120402_add_column_avaliable_quantity/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "Product" ADD COLUMN     "avaliableQuantity" INTEGER NOT NULL DEFAULT 0;
