ae64ae3fd81b72743db6fcbe5c791e9a8aa1dab5
Eduardo <eduardolagonunes@gmail.com>
2025-09-09 14:03:21 -0300

    FEAT: Adicionado BULL

--- src/user-cart/user-cart.module.ts
+++ src/user-cart/user-cart.module.ts
@@ -1,11 +1,18 @@
 import { Module } from '@nestjs/common';
+import { BullModule } from '@nestjs/bull';
 import { UserCartService } from './user-cart.service';
 import { UserCartController } from './user-cart.controller';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { SupabaseService } from 'src/common/updateImage/supabase/supabase.service';
 
 @Module({
+  imports: [
+    BullModule.registerQueue({
+      name: 'cancelation-orders',
+    }),
+  ],
   controllers: [UserCartController],
   providers: [UserCartService, PrismaService, SupabaseService],
+  exports: [UserCartService],
 })
 export class UserCartModule {}
